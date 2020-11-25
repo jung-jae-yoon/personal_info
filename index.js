861559ee6939f6f20a44c08390443e09ffb6b0e2
Yanghyeondong <hdyang0686@naver.com>
2020-11-25 14:28:49 +0900

    [FIX] 어레이 문제 해결

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Table=require('cli-table3');
 const record=[
     //['Name', 'Github Link', 'Github Static Page Link']
-    ['이현재', 'https://github.com/dlguswo333', 'http://dlguswo333.github.io/']
+    ['이현재', 'https://github.com/dlguswo333', 'http://dlguswo333.github.io/'],
     ['양현동', 'https://github.com/Yanghyeondong', 'https://yanghyeondong.github.io/']
 ];
 var table=new Table({
