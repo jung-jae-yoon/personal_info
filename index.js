2d98230ccb7184b4fa8e2e2497ac46ebd99108e5
jeonyuntae <jyt0131@gmail.com>
2020-11-25 16:18:27 +0900

    ADD 전윤태

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Table=require('cli-table3');
 const record=[
     //['Name', 'Github Link', 'Github Static Page Link']
     ['이현재', 'https://github.com/dlguswo333', 'http://dlguswo333.github.io/'],
-    ['양현동', 'https://github.com/Yanghyeondong', 'https://yanghyeondong.github.io/']
+    ['양현동', 'https://github.com/Yanghyeondong', 'https://yanghyeondong.github.io/'],
+    ['전윤태', 'https://github.com/JEONYUNTAE', 'https://jeonyuntae.github.io/']
 ];
 var table=new Table({
     head: ['Name', 'Github Link', 'Github Static Page Link']
